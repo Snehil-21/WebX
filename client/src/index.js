6d2df387372353620edb2913c49b71a1835d046e
Snehil Gupta <snehilgupta92@gmail.com>
2021-09-11 23:58:45 +0530

    Client side changes

---
+++ client/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// function App() {
+//   const persistConfig = {
+//     key: 'root',
+//     storage: storage,
+//   };
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ client/src/store/actions/Auth.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export AUTHENTICATE_USER = 'AUTHENTICATE_USER';
+
+export const signupUser = (fullName, email, username, password) => {
+    return async() => {
+        
+    }
+}
